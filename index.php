ab03e21bf29fa76a1e350eda01cf07c45ec31b21
Fredrik Leemann <39531888+freddan88@users.noreply.github.com>
2018-09-01 18:22:17 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,100 @@
+<?php
+error_reporting(0);
+header("Refresh:10");
+# Simple srcds query based on s1lk [PHP] Simple server query
+# URL: https://oxidemod.org/threads/php-simple-server-query.2374
+# Configuration:
+$ip = '192.168.0.5';
+$queryport = 27015;
+####################################################################
+$socket = @fsockopen("udp://".$ip, $queryport , $errno, $errstr, 5);
+
+stream_set_timeout($socket, 1);
+stream_set_blocking($socket, TRUE);
+fwrite($socket, "\xFF\xFF\xFF\xFF\x54Source Engine Query\x00");
+$response = fread($socket, 4096);
+@fclose($socket);
+
+$packet = explode("\x00", substr($response, 6), 5);
+$server = array();
+
+$server['name'] = $packet[0];
+$server['map'] = $packet[1];
+$server['game'] = $packet[2];
+$server['description'] = $packet[3];
+$inner = $packet[4];
+
+$server['players'] = ord(substr($inner, 2, 1));
+$server['playersmax'] = ord(substr($inner, 3, 1));
+$server['password'] = ord(substr($inner, 7, 1));
+$server['vac'] = ord(substr($inner, 8, 1));
+$file="img/maps/$server[map].jpg";
+
+if ($server['game']) {
+	$status="<span style='color:green;'>Server Online</span>";
+} else {
+	$status="<span style='color:red;'>Server Offline</span>";
+}
+
+if (file_exists($file)) {
+    $img="$file";
+} else {
+    $img="img/maps/srcds.jpg";
+}
+
+if ($server['password']==1) {
+	$password="Yes";
+} else {
+	$password="No";
+}
+
+if ($server['vac']==1) {
+	$vac="VAC Enabled";
+} else {
+	$vac="VAC Disabled";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Simple Srcds Monitor</title>
+		<link href="style.css" type="text/css" rel="stylesheet"/>
+        <meta name="robots" content="noindex,nofollow"/>
+		<meta charset="UTF-8"/>
+    </head>
+	
+    <body>
+        <div class="main-box">
+            <header><h2>Simple Srcds Monitor</h2></header>
+            <main>
+                <div class="flex">
+                    <div class="flex-1">
+                        <div>
+                            <img src="<?php echo $img ?>"/>
+                            <h4><?php echo $server['map']; ?></h4>
+                        </div>
+                    </div>
+                    <div class="flex-2">
+                        <h3>Status: <?php echo $status ?></h3>
+                        <h3>Name: <?php echo $server['name']; ?></h3>
+                        <br/>
+                        <h3>Game: <?php echo $server['description']; ?></h3>
+                        <h3>Map: <?php echo $server['map']; ?></h3>
+                        <h3>Players: <?php echo $server['players']; ?>/<?php echo $server['playersmax']; ?></h3>
+                        <br/>                        
+                        <h3>Password: <?php echo $password; ?></h3>
+                        <h3>Secure: <?php echo $vac; ?></h3>
+                    </div>
+                </div>
+				</br>
+				<center><a href="steam://connect/<?php echo $ip ?>:<?php echo $queryport ?>"><h4>Click here to connect</h4></center></a>
+            </main>
+            
+			<footer>
+				<h2>Simple Srcds Monitor</h2>
+			</footer>
+        </div>
+    </body>
+</html>
---
+++ readme.txt
@@ -0,0 +1,68 @@
+------------
+START README
+------------
+
+Updated: 2018-09-01
+
+------
+Links:
+------
+
+URL: http://www.leemann.se/fredrik
+Donate: https://www.paypal.me/freddan88
+YouTube: https://www.youtube.com/user/FreLee54
+
+Github-main: https://github.com/freddan88
+Github-page: 
+
+Video:
+Tutorial:
+Download:
+
+Platform: WEB
+Language: HTML CSS PHP
+
+------------
+Description:
+------------
+
+Simple SRCDS Monitor is a webpage that will monitor and query Valves Dedicated Source [ SRCDS ] Servers by using php
+The code that connect to the server is based on s1lk´s Simple server query
+URL: https://oxidemod.org/threads/php-simple-server-query.2374
+
+I have edited and added to this code and built a website around it to display a picture of current map and more
+This webpage will only work if your webhost supports php-sockets and is not blocking any communication...
+
+-------------
+Instructions:
+------------
+
+1.
+Edit the configuration is index.php to include your IP and port for the game server
+Example:
+	# Configuration:
+	$ip = '192.168.0.5';
+	$queryport = 27015;
+	
+	OR
+	
+	# Configuration:
+	$ip = '82.10.41.5';
+	$queryport = 27015;
+	
+2.
+Upload index.php, style.css and the folder 'img' to your webhost and visit the webpage
+
+------------------
+Actions and usage:
+------------------
+
+You can upload more pictures of maps in 'img/maps'
+Simple SRCDS Monitor only supports .jpg-files and will display a picture that is having the same name as the current map playing on SRCDS
+If a picture cant be found the default picture from 'img/maps/srcds.jpg' will be used...
+	
+This monitoring page has only tested on SRCDS servers that are playing Counter Strike Source
+	
+----------
+END README
+----------
